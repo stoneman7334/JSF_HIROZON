c3f656235af60fdd54d78b85abf1f2aaac7a3c91
ohba_yuichi <nepinepi984@gmail.com>
2017-10-03 23:45:47 +0900

    入力したPASSのSHA256ハッシュ化のロジックの実装

--- src/java/beans/UserBean.java
+++ src/java/beans/UserBean.java
@@ -5,6 +5,11 @@
  */
 package beans;
 
+import ejb.UserDb;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 
@@ -16,13 +21,54 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class UserBean{
 
+	@EJB
+	private UserDb userDb;
+
 	private String id;
 	private String pass;
 	
-    /**
-     * Creates a new instance of UserBean
-     */
     public UserBean() {
     }
+
+	public UserDb getUserDb() {
+		return userDb;
+	}
+	public void setUserDb(UserDb userDb) {
+		this.userDb = userDb;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	//*** ログインチェックを行うメソッド ***//
+	public String loginCheck() throws NoSuchAlgorithmException{
+		System.out.println("call loginCheck()");
+		//*** this.pass を、SHA256でハッシュ化する ***//
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		byte[] result = md.digest(this.pass.getBytes());	//*** 256ハッシュ化した結果をバイト配列に代入する ***//
+		
+		StringBuilder sb = new StringBuilder();
+		for (byte b : result){
+			sb.append(String.format("%02x", b));
+		}
+		System.out.println(sb.toString());	//*** 入力したpassをSHA256ハッシュ化した結果の文字列 ***//
+		
+		//*** ここで、DB検索してユーザのチェックを行う ***//
+		
+		return "account_menu"; //*** 暫定 ***//
+	}
+	
+	
+	
+	
     
 }
