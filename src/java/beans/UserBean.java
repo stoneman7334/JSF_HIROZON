e586616f4d1907c4378fb4ea2a35d1c6e5cc96b6
ohba_yuichi <nepinepi984@gmail.com>
2017-10-04 17:52:08 +0900

    user add logic coding

--- src/java/beans/UserBean.java
+++ src/java/beans/UserBean.java
@@ -22,120 +22,175 @@ import model.User;
 @RequestScoped
 public class UserBean {
 
-    @EJB
-    private UserDb userDb;
+	@EJB
+	private UserDb userDb;
 
-    private String id;
-    private String pass;
-    
-   //*** 変更前アドレスと変更予定のアドレス ***//
-    private String beMail;
-    private String newMail;
-    
-    User user;
-    
-    
+	private String id;				//*** ユーザID ***//
+	private String pass;			//*** パスワード ***//
+	private String u_name;			//*** 氏名 ***//
+	private String u_mailaddr;		//*** メールアドレス ***//
+	private String u_post;			//*** 郵便番号 ***//
+	private String u_address;		//*** 住所 ***//
+	private String u_tel;			//*** 電話番号 ***//
+	private String u_birth_day;		//*** 生年月日 ***//
+	private String u_sex;			//*** 性別 ***//
+	//*** 変更前アドレスと変更予定のアドレス ***//
+	private String beMail;
+	private String newMail;
 
-    public UserBean() {
-    }
+	User user;
 
-    public String getBeMail() {
-        return beMail;
-    }
+	public UserBean() {
+	}
 
-    public void setBeMail(String beMail) {
-        this.beMail = beMail;
-    }
+	public String getBeMail() {
+		return beMail;
+	}
+	public void setBeMail(String beMail) {
+		this.beMail = beMail;
+	}
+	public String getNewMail() {
+		return newMail;
+	}
+	public void setNewMail(String newMail) {
+		this.newMail = newMail;
+	}
+	public UserDb getUserDb() {
+		return userDb;
+	}
+	public void setUserDb(UserDb userDb) {
+		this.userDb = userDb;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getU_name() {
+		return u_name;
+	}
+	public void setU_name(String u_name) {
+		this.u_name = u_name;
+	}
+	public String getU_mailaddr() {
+		return u_mailaddr;
+	}
+	public void setU_mailaddr(String u_mailaddr) {
+		this.u_mailaddr = u_mailaddr;
+	}
+	public String getU_post() {
+		return u_post;
+	}
+	public void setU_post(String u_post) {
+		this.u_post = u_post;
+	}
+	public String getU_address() {
+		return u_address;
+	}
+	public void setU_address(String u_address) {
+		this.u_address = u_address;
+	}
+	public String getU_tel() {
+		return u_tel;
+	}
+	public void setU_tel(String u_tel) {
+		this.u_tel = u_tel;
+	}
+	public String getU_birth_day() {
+		return u_birth_day;
+	}
+	public void setU_birth_day(String u_birth_day) {
+		this.u_birth_day = u_birth_day;
+	}
+	public String getU_sex() {
+		return u_sex;
+	}
+	public void setU_sex(String u_sex) {
+		this.u_sex = u_sex;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    public String getNewMail() {
-        return newMail;
-    }
+	//*** ログインチェックを行うメソッド ***//
+	public String loginCheck() throws NoSuchAlgorithmException {
+		System.out.println("call loginCheck()");
 
-    public void setNewMail(String newMail) {
-        this.newMail = newMail;
-    }
-    
-    
+		System.out.println(String.format("id=%s : pass=%s", id, pass));
+		//*** this.pass を、SHA256でハッシュ化する ***//
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		byte[] result = md.digest(this.pass.getBytes());	//*** 256ハッシュ化した結果をバイト配列に代入する ***//
 
-    public UserDb getUserDb() {
-        return userDb;
-    }
-    public void setUserDb(UserDb userDb) {
-        this.userDb = userDb;
-    }
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getPass() {
-        return pass;
-    }
-    public void setPass(String pass) {
-        this.pass = pass;
-    }
+		StringBuilder sb = new StringBuilder();
+		for (byte b : result) {
+			sb.append(String.format("%02x", b));
+		}
+		System.out.println(sb.toString());	//*** 入力したpassをSHA256ハッシュ化した結果の文字列 ***//
 
-    //*** ログインチェックを行うメソッド ***//
-    public String loginCheck() throws NoSuchAlgorithmException {
-        System.out.println("call loginCheck()");
+		user = userDb.find(id, sb.toString());
+		if (user == null) {
+			return "";
+		}
+		//*** ここで、DB検索してユーザのチェックを行う ***//
+		return "account_menu"; //*** 暫定 ***//
+	}
 
-        System.out.println(String.format("id=%s : pass=%s", id, pass));
-        //*** this.pass を、SHA256でハッシュ化する ***//
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        byte[] result = md.digest(this.pass.getBytes());	//*** 256ハッシュ化した結果をバイト配列に代入する ***//
+	//*** 新規会員登録を行うメソッド ***//
+	public String addUser() {
+		System.out.println("call UserBean->addUser()");
+		//*** ユーザIDの重複をチェックする ***//
+		if (!userDb.checkDuplicateUserId(id)) {
+			return "";	//*** 処理を抜ける ***//
+		}
+		//*** 入力した値たちを取得する ***//
+		User u = new User(
+				id,
+				u_name,
+				pass,
+				u_mailaddr,
+				u_address,
+				u_birth_day,
+				u_post,
+				u_sex,
+				u_tel
+		);
+		//*** DBに新規登録をかける ***//
 
-        StringBuilder sb = new StringBuilder();
-        for (byte b : result) {
-            sb.append(String.format("%02x", b));
-        }
-        System.out.println(sb.toString());	//*** 入力したpassをSHA256ハッシュ化した結果の文字列 ***//
-        
-        user = userDb.find(id,sb.toString());
-        if (user == null){
-            return "";
-        }
-        //*** ここで、DB検索してユーザのチェックを行う ***//
-        return "account_menu"; //*** 暫定 ***//
-    }
-    
-    //*** 新規会員登録を行うメソッド ***//
-    public String addUser(){
-        System.out.println("call UserBean->addUser()");
-        //*** 入力した値たちを取得する ***//
-        
-        //*** DBに新規登録をかける ***//
-        
-        //*** その結果に応じた画面遷移先を設定する ***//
-        
-        return "";  //*** 暫定 ***//
-    }
-    
-    //*** 指定IDのユーザのパスワードをリセットして、仮パスワードを指定するメソッド ***//
-    public String passForget(){
-        System.out.println("call UserBean->passForget()");
-        //*** 入力してきたIDのユーザが存在するか確認 確認できたら、処理続行 ***//
-        
-        //*** ランダムな文字列生成（8文字くらい） ***//
-        
-        //*** 生成した文字列を、DBに格納する ***//
-        
-        return "";  //*** ページ遷移はなしで、画面に、仮パスワードを出力する ***//
-    }
-    
-    public String changeMail() throws NoSuchAlgorithmException{
-        String res = null;
-        //*** アドレス変更の際のパスワードのハッシュ処理 ***//
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        byte[] result = md.digest(this.pass.getBytes());
-        StringBuilder sb = new StringBuilder();
-        for (byte b : result) {
-            sb.append(String.format("%02x", b));
-        }
-        
-        
-        
-        return res;
-    }
+		//*** その結果に応じた画面遷移先を設定する ***//
+		return "";  //*** 暫定 ***//
+	}
+
+	//*** 指定IDのユーザのパスワードをリセットして、仮パスワードを指定するメソッド ***//
+	public String passForget() {
+		System.out.println("call UserBean->passForget()");
+		//*** 入力してきたIDのユーザが存在するか確認 確認できたら、処理続行 ***//
+
+		//*** ランダムな文字列生成（8文字くらい） ***//
+		//*** 生成した文字列を、DBに格納する ***//
+		return "";  //*** ページ遷移はなしで、画面に、仮パスワードを出力する ***//
+	}
+
+	public String changeMail() throws NoSuchAlgorithmException {
+		String res = null;
+		//*** アドレス変更の際のパスワードのハッシュ処理 ***//
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		byte[] result = md.digest(this.pass.getBytes());
+		StringBuilder sb = new StringBuilder();
+		for (byte b : result) {
+			sb.append(String.format("%02x", b));
+		}
+
+		return res;
+	}
 
 }
--- src/java/ejb/UserDb.java
+++ src/java/ejb/UserDb.java
@@ -20,27 +20,41 @@ import model.User;
 @Stateless
 public class UserDb {
 
-    @PersistenceContext(unitName = "HirozonPU")
-    private EntityManager em;
+	@PersistenceContext(unitName = "HirozonPU")
+	private EntityManager em;
 
-    public void persist(User u) {
-        em.persist(u);
-    }
+	public void persist(User u) {
+		em.persist(u);
+	}
 
-    public User find(String id, String pass) {
-        User u = null;
+	//*** 指定したID passのユーザのインスタンスを返すメソッド ***//
+	public User find(String id, String pass) {
+		User u = null;
 
-        TypedQuery<User> query = em.createQuery("select u from User u where u.u_Id = ?1 and u.u_pass = ?2", User.class); //***  ***//
-        query.setParameter(1, id);      //***  ***//
-        query.setParameter(2, pass);    //***  ***//
+		TypedQuery<User> query = em.createQuery("select u from User u where u.u_Id = ?1 and u.u_pass = ?2", User.class); //***  ***//
+		query.setParameter(1, id);      //*** IDをセット ***//
+		query.setParameter(2, pass);    //*** pass（ハッシュ済み）をセット ***//
 
-        u = query.getSingleResult();    //***  ***//
+		u = query.getSingleResult();    //*** 結果の取得 ***//
 
-        return u;      //***  ***//
-    }
-    
-    public User merge(User user){
-        em.merge(user);
-        return user;
-    }
+		return u;      //*** Userクラスのインスタンスを返す ***//
+	}
+
+	//*** 指定されたユーザIDに重複がないかチェックするメソッド ***//
+	public boolean checkDuplicateUserId(String id) {
+		TypedQuery<User> query = em.createQuery(
+				"select u from User u where u.u_Id = ?1", User.class);
+		query.setParameter(1, id);			//***  ***//
+		User u = query.getSingleResult();	//***  ***//
+		if (u == null) {
+			return true;		//***  ***//
+		} else {
+			return false;		//***  ***//
+		}
+	}
+
+	public User merge(User user) {
+		em.merge(user);
+		return user;
+	}
 }
